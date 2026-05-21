30d3eb16fa5a95e371b01bd7a7adcfa29c524842
abeltyr <abeltyr@gmail.com>
2026-05-21 16:01:19 +0300

    refactor: First itteration rebuild of the config

--- packages/utils/config/src/loader/database.ts
+++ packages/utils/config/src/loader/database.ts
@@ -1,20 +1,36 @@
-import { initPool, getPool, closePool } from "@damatjs/orm-connector";
-import type { Pool, DbPoolConfig } from "@damatjs/orm-type";
+import { ConnectionManager } from "@damatjs/orm-pg";
+import { ModuleServiceBase } from "@damatjs/services";
+import type { Pool } from "@damatjs/deps/pg";
+import type { ModuleConfig } from "../types";
 
-export async function initDatabase<TModules extends readonly any[]>(
-  { databaseUrl, modules }: {
-    databaseUrl: string | DbPoolConfig,
-    modules: TModules
-  }
-): Promise<Pool> {
+let connectionManager: ConnectionManager | null = null;
 
-  const pool = await initPool(databaseUrl);
-
-  for (const mod of modules) {
-    mod.init(() => getPool());
-  }
-
+export async function initDatabase(dbUrl: string): Promise<Pool> {
+  connectionManager = new ConnectionManager(dbUrl);
+  const pool = await connectionManager.connect();
+  ModuleServiceBase.init(pool);
   return pool;
 }
 
-export { getPool, closePool };
+// TODO: not seeing the aim or usefulness of this Will need to remove or update
+export async function loadModules(modules: ModuleConfig[]): Promise<void> {
+  for (const mod of modules) {
+    const moduleExports = await import(mod.resolve);
+    if (moduleExports.default && typeof moduleExports.default === "function") {
+      const ServiceClass = moduleExports.default;
+      new ServiceClass();
+    }
+  }
+}
+
+// TODO: we are moving away from the orm-pg base pool setup so this and the close need a redo
+export function getConnectionManager(): ConnectionManager | null {
+  return connectionManager;
+}
+
+export async function closeDatabase(): Promise<void> {
+  if (connectionManager) {
+    await connectionManager.disconnect();
+    connectionManager = null;
+  }
+}
